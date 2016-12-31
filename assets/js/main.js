2361df81617cf191ad50f78f8f21c460f00761a2
Sarah Kidd Hubbard <sarahjkidd@gmail.com>
2016-12-31 09:26:28 -0500

    move javascript to file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,5 @@
-
+$(document).ready (function () {
+  $(window).on("scroll touchmove", function () {
+    $('header, nav li, nav ul, #logo, #logo-mobile').toggleClass('smaller', $(document).scrollTop() > 100);
+  });
+});
